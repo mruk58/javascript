4829e44ed48cb5218364e550674c91c46d98359d
Usama khan <101790205+mruk58@users.noreply.github.com>
2024-12-02 18:53:21 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 142536
+
+let accountEmail = "usamamajidpst@gmail.com"
+var accountPassword = "2356428"
+accountCity = "Peshawar"
+
+ //const accountId = 345734 // not allowed   
+
+accountEmail = "fr5541422@gmail.com" 
+accountPassword = "23647892" 
+accountCity = "Nowshera"
+
+console.log(accountId)
+
+ console.table([accountId, accountEmail, accountPassword, accountCity]);
+  
+
